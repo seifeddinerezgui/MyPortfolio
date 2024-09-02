009215c9ce3c4ddfc634a42ce74e3c9d9ab733e6
seif eddine rezgui <seifeddine.rezgui@esprit.tn>
2024-09-02 14:43:32 +0200

    changing index paths

---
+++ static/logos/skills.ts
@@ -0,0 +1,195 @@
+import Assets from './assets';
+import type { Skill, SkillCategory } from '../types';
+import svelte from '../md/svelte.md?raw';
+import { omit, type StringWithAutoComplete } from '@riadh-adrani/utils';
+
+const defineSkillCategory = <S extends string>(data: SkillCategory<S>): SkillCategory<S> => data;
+
+const categories = [
+	defineSkillCategory({ name: 'Programming Languages', slug: 'pro-lang' }),
+	defineSkillCategory({ name: 'Frameworks', slug: 'framework' }),
+	defineSkillCategory({ name: 'Libraries', slug: 'library' }),
+	defineSkillCategory({ name: 'Languages', slug: 'lang' }),
+	defineSkillCategory({ name: 'Databases', slug: 'db' }),
+	defineSkillCategory({ name: 'ORMs', slug: 'orm' }),
+	defineSkillCategory({ name: 'DevOps', slug: 'devops' }),
+	defineSkillCategory({ name: 'Testing', slug: 'test' }),
+	defineSkillCategory({ name: 'Dev Tools', slug: 'devtools' }),
+	defineSkillCategory({ name: 'Markup & Style', slug: 'markup-style' }),
+	defineSkillCategory({ name: 'Design', slug: 'design' }),
+	defineSkillCategory({ name: 'Soft Skills', slug: 'soft' })
+] as const;
+
+const defineSkill = <S extends string>(
+	skill: Omit<Skill<S>, 'category'> & {
+		category?: StringWithAutoComplete<(typeof categories)[number]['slug']>;
+	}
+): Skill<S> => {
+	const out: Skill<S> = omit(skill, 'category');
+
+	if (skill.category) {
+		out.category = categories.find((it) => it.slug === skill.category);
+	}
+
+	return out;
+};
+
+export const items = [
+	defineSkill({
+		slug: 'js',
+		color: 'yellow',
+		description:
+			'JavaScript is a versatile programming language commonly used in web development to create interactive effects within web browsers.',
+		logo: Assets.JavaScript,
+		name: 'JavaScript',
+		category: 'pro-lang'
+	}),
+	defineSkill({
+		slug: 'ts',
+		color: 'blue',
+		description:
+			'TypeScript is a statically typed superset of JavaScript that compiles to plain JavaScript. It is designed for developing large applications and transcompiles to JavaScript.',
+		logo: Assets.TypeScript,
+		name: 'TypeScript',
+		category: 'pro-lang'
+	}),
+	defineSkill({
+		slug: 'py',
+		color: 'blue',
+		description:
+			'python .',
+		logo: Assets.Python,
+		name: 'Python',
+		category: 'pro-lang'
+	}),
+	defineSkill({
+		slug: 'symfony',
+		color: 'white',
+		description:
+			'symfony .',
+		logo: Assets.Symfony,
+		name: 'Symfony',
+		category: 'framework'
+	}),
+	defineSkill({
+		slug: 'php',
+		color: 'purple',
+		description:
+			'php .',
+		logo: Assets.Php,
+		name: 'php',
+		category: 'pro-lang'
+	}),
+	defineSkill({
+		slug: 'css',
+		color: 'blue',
+		description:
+			'CSS (Cascading Style Sheets) is used to design and control the layout of web pages, ensuring they look attractive and function smoothly across different devices.',
+		logo: Assets.CSS,
+		name: 'CSS',
+		category: 'markup-style'
+	}),
+	defineSkill({
+		slug: 'html',
+		color: 'orange',
+		description:
+			'HTML (Hypertext Markup Language) is the standard markup language used to create and structure content on the web.',
+		logo: Assets.HTML,
+		name: 'HTML',
+		category: 'markup-style'
+	}),
+	defineSkill({
+		slug: 'sass',
+		color: 'pink',
+		description:
+			'Sass (Syntactically Awesome Style Sheets) is a preprocessor scripting language that is interpreted or compiled into CSS, adding power and elegance to the basic language.',
+		logo: Assets.Sass,
+		name: 'Sass',
+		category: 'markup-style'
+	}),
+	defineSkill({
+		slug: 'reactjs',
+		color: 'cyan',
+		description:
+			'React is a JavaScript library for building user interfaces, particularly single-page applications, by using a component-based architecture.',
+		logo: Assets.ReactJs,
+		name: 'React Js',
+		category: 'library'
+	}),
+	defineSkill({
+		slug: 'svelte',
+		color: 'orange',
+		description: svelte, // Assuming you want to keep the markdown content
+		logo: Assets.Svelte,
+		name: 'Svelte',
+		category: 'library'
+	}),
+	defineSkill({
+		slug: 'angular',
+		color: 'red',
+		description:
+			'Angular is a platform and framework for building single-page client applications using HTML and TypeScript. Angular is written in TypeScript.',
+		logo: Assets.Angular, // Assuming you have an Angular logo in Assets
+		name: 'Angular',
+		category: 'framework'
+	}),
+	defineSkill({
+		slug: 'springboot',
+		color: 'green',
+		description:
+			'Spring Boot is an open-source, microservice-based Java web framework that provides a robust infrastructure for developing and deploying enterprise applications.',
+		logo: Assets.springboot, // Assuming you have a Spring Boot logo in Assets
+		name: 'Spring Boot',
+		category: 'framework'
+	}),
+	defineSkill({
+		slug: 'mysql',
+		color: 'blue',
+		description:
+			'MySQL is an open-source relational database management system based on SQL (Structured Query Language), widely used for managing data in web applications.',
+		logo: Assets.mysql, // Assuming you have a MySQL logo in Assets
+		name: 'MySQL',
+		category: 'db'
+	})
+] as const;
+
+export const title = 'Skills';
+
+export const getSkills = (
+	...slugs: Array<StringWithAutoComplete<(typeof items)[number]['slug']>>
+): Array<Skill> => items.filter((it) => slugs.includes(it.slug));
+
+export const groupByCategory = (
+	query: string
+): Array<{ category: SkillCategory; items: Array<Skill> }> => {
+	const out: ReturnType<typeof groupByCategory> = [];
+
+	const others: Array<Skill> = [];
+
+	items.forEach((item) => {
+		if (query.trim() && !item.name.toLowerCase().includes(query.trim().toLowerCase())) return;
+
+		// push to others if item does not have a category
+		if (!item.category) {
+			others.push(item);
+			return;
+		}
+
+		// check if category exists
+		let category = out.find((it) => it.category.slug === item.category?.slug);
+
+		if (!category) {
+			category = { items: [], category: item.category };
+
+			out.push(category);
+		}
+
+		category.items.push(item);
+	});
+
+	if (others.length !== 0) {
+		out.push({ category: { name: 'Others', slug: 'others' }, items: others });
+	}
+
+	return out;
+};
